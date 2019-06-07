cc23b193bbdd1665d6d88ad5e7375942e0a947af
Tia Rose <shortyshort789@aol.com>
2019-06-06 20:14:29 -0700

    added jsdocs to all required files

--- src/middleware/404.js
+++ src/middleware/404.js
@@ -1,5 +1,11 @@
 'use strict';
 
+/**
+ * @module 404
+ * @param {object} req - the request object
+ * @param {object} res - the response object
+ * @desc client error  
+ */
 module.exports = (req,res,next) => {
   let error = { error: 'Resource Not Found' };
   res.status(404).json(error).end();
--- src/models/categories/categories-model.js
+++ src/models/categories/categories-model.js
@@ -1,15 +1,16 @@
 'use strict';
 
-/**
- * 
- */
-
 const Model = require('../memory-model.js');
 
 const schema = {
   _id: {required:true},
   name: {required:true},
 };
+
+/**
+ * @class Categories
+ * @desc Categories class which is inherited from the Model Class
+ */
 
 class Categories extends Model {}
 
